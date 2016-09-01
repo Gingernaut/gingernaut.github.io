8d31fa8d6ac69ab5d9930bde8648b01e2f275835
Tyler Peterson <tylermpeterson36@gmail.com>
2016-09-01 21:31:57 +0000

    new design, rewrote a lot

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,12 @@
-// $(document).ready(function () {
-//     $('#landing').mousemove(function (e) {
-//         parallax(e, this, 1);
-//         parallax(e, document.getElementById('landingtextcenter'), 2);
-//         parallax(e, document.getElementById('layer-three'), 3);
-//     });
-// });
-
-// function parallax(e, target, layer) {
-//     var layer_coeff = 30 / layer;
-//     var x = ($(window).width() - target.offsetWidth) / 2 - (e.pageX - ($(window).width() / 2)) / layer_coeff;
-//     var y = ($(window).height() - target.offsetHeight) / 2 - (e.pageY - ($(window).height() / 2)) / layer_coeff;
-//     $(target).offset({ top: y ,left : x });
-// };
+$(document).ready(function() {
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var target = this.hash;
+        var $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+});

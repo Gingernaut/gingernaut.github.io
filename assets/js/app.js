42c577f4cb52ee4b8dcb532907dd8babda5c7484
Tyler Peterson <tylermpeterson36@gmail.com>
2016-09-27 14:38:18 -0600

    updated js to (hopefully) begin baffle faster

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,45 +1,5 @@
 $(document).ready(function() {
 
-    'use strict';
-    var a = baffle("#a");
-    var b = baffle("#b");
-    var c = baffle("#c");
-    var d = baffle("#d");
-    var e = baffle("#e");
-    var f = baffle("#f");
-
-
-    a.set({
-        characters: '▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒█',
-    });
-
-    b.set({
-        characters: '▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒█',
-    });
-
-    c.set({
-        characters: '▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒█',
-    });
-
-    d.set({
-        characters: '▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒█',
-    });
-
-    e.set({
-        characters: '▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒█',
-    });
-
-    f.set({
-        characters: '▓░█ ▒░▒▓░ ▒░░▓> ▒█▓ █░><▒ █▒█▓ ▓░/ ▓▓/█ █▓▒█',
-    });
-
-    b.reveal(1000);
-    a.reveal(1000);
-    c.reveal(1000);
-    d.reveal(9000);
-    e.reveal(9000);
-    f.reveal(9000);
-
     $('a[href^="#"]').on('click', function(e) {
         e.preventDefault();
         var target = this.hash;

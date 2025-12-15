9aa8d97102773f59a25fb734925277f7569737fd
Tyler Peterson <tylermpeterson36@gmail.com>
2025-12-14 21:21:31 -0800

    remove twitter

--- docs/.vuepress/components/AboutPage.vue
+++ docs/.vuepress/components/AboutPage.vue
@@ -16,24 +16,6 @@
     </div>
 
     <div id="socialLinks">
-      <a href="https://twitter.com/gingernaut" target="_blank" rel="noopener noreferrer">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="42"
-          height="42"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="#1DA1F2"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="feather feather-twitter"
-        >
-          <path
-            d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.42 11.42 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"
-          />
-        </svg>
-      </a>
       <!-- Note: fix padding/margin for hover -->
       <a href="https://github.com/gingernaut" target="_blank" rel="noopener noreferrer">
         <svg
